97ef69f8ce67fd589869fe5e8ca81746d33d0aca
Stephen Hogsten <stephenhogsten@gmail.com>
2018-07-08 23:21:43 -0400

    added all old news items

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -168,10 +168,109 @@ class PagesController < ApplicationController
         link: "https://modernmrsdarcy.com/133-episode/",
         img_src: "https://modernmrsdarcy.com/wp-content/uploads/2017/03/mmd-logo-handmade-400.png",
         source: "Modern Mrs. Darcy",
-        title: "Podcast Review",
-      }
-
+        title: "Podcast Review"
+      },
+      {
+        date: Date.new(2018, 5, 25),
+        link: "https://www.mydomaine.com/best-new-books-for-summer-2018--5b05bc439f958",
+        img_src: "data:image/svg+xml;charset=utf-8,%3Csvg viewBox='0 0 169 16' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cpath d='M0 .083h168.914V15.99H0V.083z'/%3E%3Cpath d='M44.094 12.982c2.463 0 4.47-2.033 4.47-4.528 0-2.495-2.007-4.528-4.47-4.528H41.61v9.056h2.484zM38.632 16V.908h5.462c4.112 0 7.448 3.38 7.448 7.546 0 4.165-3.336 7.546-7.448 7.546h-5.462zm21.253-7.546c0 2.495 2.007 4.528 4.47 4.528 2.462 0 4.47-2.033 4.47-4.528 0-2.495-2.008-4.528-4.47-4.528-2.463 0-4.47 2.033-4.47 4.528zM64.355 16c-4.112 0-7.45-3.38-7.45-7.546 0-4.165 3.338-7.546 7.45-7.546 4.11 0 7.448 3.38 7.448 7.546 0 4.165-3.337 7.546-7.45 7.546zM79.73.908h.753l5.383 5.896L91.23.908h.774L93.136 16h-2.98L89.5 7.267l-3.634 3.984-3.636-3.98-.672 8.73h-2.98L79.73.91m30.746 9.558l-2.204-4.105-2.205 4.11h4.41zm1.63 3.02h-7.668L103.088 16H99.69L108.273.083l8.6 15.917h-3.397l-1.37-2.515zm11.72-12.58h2.98V15.99h-2.98V.908zm12.114 0h.795l9.137 8.814V.908h2.98V16h-.815l-9.117-8.814V16h-2.98V.908m22.05 0h10.924v3.018h-7.946v4.528h5.96v3.018h-5.96v1.51h7.946V16H157.99V.908' fill='%23000' mask='url%28%23mask-2%29'/%3E%3Cpath d='M12.167 14.563l-.855-9.693L7.098 9.3 2.86 4.867l-.775 9.697-1.033-.017.862-12.424 5.153 5.73 5.17-5.763.923 12.456-.993.017zm.91-13.918h-.743l-5.26 5.64L1.88.645h-.744L0 15.49h2.937l.656-8.615 3.482 3.816 3.514-3.82.68 8.62h2.936L13.08.65zm14.19 9.365v4.82h-1.302v-4.82L20.88 1.634h1.416l4.317 7.087 4.397-7.08h1.42l-5.16 8.378zm3.21-9.316l-3.864 6.24L22.77.694l-3.685.01 5.904 9.64v5.475h3.25v-5.48L34.18.7 30.475.69zM24.89 8.77l.003.005-.003-.005' fill='%23000'/%3E%3Cpath fill='%23000' d='M24.89 8.77v-.023l.003.028-.003-.005'/%3E%3C/g%3E%3C/svg%3E",
+        source: "My Domaine",
+        title: "The Most Anticipated Books of Summer 2018 That Are Actually Worth Your Time"
+      },
+      {
+        date: Date.new(2018, 5, 16),
+        link: "https://twitter.com/iBooks/status/996799975102263297",
+        img_src: "https://pbs.twimg.com/profile_images/804804365093511168/cawEsoVo_bigger.jpg",
+        source: "iBooks",
+        title: "Summer's Most Anticipated Books"
+      },
+      {
+        link: "https://creativewriting.wisc.edu/fellows.html",
+        img_src: "https://creativewriting.wisc.edu/images/UW-CWP_header.gif",
+        source: "Creative Writing at the University of Wisconsin-Madison",
+        title: "Lucy Tan awarded the James C. McCreight Fellowship from the Wisconsin Institute of Creative Writing"
+      },
+      {
+        link: "https://best-books.publishersweekly.com/pw/best-books/summer-reads-2018/fiction#book/book-12",
+        img_src: "https://www.publishersweekly.com/images/logo-trans.png",
+        source: "Publisher's Weekly",
+        title: "What We Were Promised Named a \"Best Book of Summer\""
+      },
+      {
+        date: Date.new(2018, 3, 27),
+        link: "https://www.barnesandnoble.com/blog/announcing-discover-great-new-writers-summer-2018-selections/",
+        img_src: "http://www.barnesandnobleinc.com/wp-content/uploads/logo.svg",
+        source: "Barnes & Noble",
+        title: "Discover Great New Writers Summer 2018 Selection"
+      },
+      {
+        date: Date.new(2018, 4, 17),
+        link: "https://www.kirkusreviews.com/book-reviews/lucy-tan/what-we-were-promised/",
+        img_src: "https://d3eoifnsb8kxf0.cloudfront.net/img/kirkus-nav-logo.svg",
+        source: "Kirkus",
+        title: "Kirkus reviews What We Were Promised"
+      },
+      {
+        link: "https://www.southernliving.com/culture/summer-books-2018",
+        img_src: "https://www.southernliving.com/sites/all/themes/ti_sl_anniversary/assets/images/sl-logo.svg",
+        source: "Southern Living",
+        title: "The Best New Books Coming Out Summer 2018"
+      },
+      {
+        date: Date.new(2018, 5, 23),
+        link: "https://shereads.com/this-summers-must-read-debuts/",
+        img_src: "http://5gluc3z8809473hrbtk76i16qu.wpengine.netdna-cdn.com/wp-content/uploads/2018/06/she-reads-full-banner-simple-1.jpg",
+        source: "She Reads",
+        title: "This Summer's Must-Read Debuts"
+      },
+      {
+        date: Date.new(2018, 1, 18),
+        link: "https://lunch.publishersmarketplace.com/2018/01/buzz-books-spring-summer-preview-literary-debut-fiction/",
+        img_src: "https://32fc87n66z83fzh742e992fv-wpengine.netdna-ssl.com/wp-content/uploads/2017/08/Publishers-Lunch-Logo-master.png",
+        source: "Publisher's Marketplace",
+        title: "Buzz Books Spring/Summer Preview: Literary & Debut Fiction"
+      },
+      {
+        date: Date.new(2018, 1, 18),
+        link: "http://reviews.libraryjournal.com/2018/01/prepub/fiction-previews/summer-debuts-fiction-previews-jul-2018/",
+        img_src: "https://s3.amazonaws.com/ImageCloud/logo_LibraryJournal_242x32_darker.png",
+        source: "Library Journal",
+        title: "Summer Debuts | Fiction Previews, Jul. 2018"
+      },
+      {
+        date: Date.new(2017, 12, 27),
+        link: "https://www.bustle.com/p/19-debut-novels-coming-out-in-2018-that-you-definitely-wont-want-to-miss-7649015",
+        img_src: "https://cdn.bustle.com/bustle/production/public/logo-5f0141.svg",
+        source: "Bustle",
+        title: "19 Debut Novels Coming Out In 2018 That You Definitely Won’t Want To Miss"
+      },
+      {
+        date: Date.new(2017, 12, 26),
+        background: "black",
+        link: "https://www.huffingtonpost.com/entry/2018-book-preview_us_5a383493e4b0c65287aba20b",
+        img_src: "https://s.m.huffpost.com/assets/HuffPostHero-d783f680fb974e80346bdfa145131d058e596e1d32f791981875aa481702c770.svg",
+        source: "Huffington Post",
+        title: "60 Books We Can’t Wait To Read In 2018"
+      },
+      {
+        date: Date.new(2017, 12, 26),
+        link: "https://electricliterature.com/46-books-by-women-of-color-to-read-in-2018-70a0bf5bf4f2",
+        img_src: "https://cdn-images-1.medium.com/letterbox/480/72/50/50/1*dKJR-1lOwP9XFYKzLvl-Iw.png?source=logoAvatar-be69ce71416c---e470410858b4",
+        source: "Electric Literature",
+        title: "46 Books By Women of Color to Read in 2018"
+      }      
     ]
+
+    @interviews = [
+      {
+        date: Date.new(2017, 9, 30),
+        link: "https://soundcloud.com/jackson-tobin-922865676/episode-five-book-deal-w-lucy-tan",
+        img_src: "https://is2-ssl.mzstatic.com/image/thumb/Music128/v4/5a/38/3d/5a383d73-bcd3-088a-114c-863e59e18c7a/source/170x170bb.jpg",
+        source: "Write & Eat Podcast",
+        title: "Podcast Interview"
+      }
+    ]
+
   end
 
   def events
